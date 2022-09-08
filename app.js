87ed1d48eb135134767c78a5a230b72ea8d98472
zzinue <zzinue@gmail.com>
2022-09-08 14:47:43 -0500

    adding logic

--- app.js
+++ app.js
@@ -3,7 +3,46 @@ const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
+let result
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
+    generatedComputerChoice()
+    getResult()
 }))
+const generatedComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+    console.log(randomNumber);
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+const getResult = () => {
+    if (computerChoice === userChoice) {
+        result = 'its a draw!'
+    }
+    if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = 'You win!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'scissors') {
+        result = 'You win!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = 'You lost!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = 'You win!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
+        result = 'You lost!'
+    }
+    resultDisplay.innerHTML = result
+}
