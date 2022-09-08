dcfb1c9b1f7bbff32cc0a769d8a41ed7d5d54bd1
zzinue <zzinue@gmail.com>
2022-09-07 23:18:00 -0500

    html buttons layout

---
+++ app.js
@@ -0,0 +1,3 @@
+const computerChoice = document.getElementById('computer-choice')
+const userChoice = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')  
