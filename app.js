ad0aeb39c6e01899c3b438af65ba4eb39717feca
zzinue <zzinue@gmail.com>
2022-09-07 23:25:06 -0500

    adding addeventlistener to the buttons

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const computerChoice = document.getElementById('computer-choice')
-const userChoice = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')  
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+}))
